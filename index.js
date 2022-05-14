6d81b48646d4864aed44f7ca5146525ab5b9eca8
Dakiee <ldavaanyam@gmail.com>
2022-05-14 21:01:24 +0800

    server

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const fs = require("fs");
+const urlObj = require("url");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let urlPath = req.url.split("/")[1];
+  console.log("html unshihiin umnu");
+  fs.readFile(urlPath, (err, data) => {
+    if (err) {
+      res.setHeader("Content-type", "text/html");
+      res.write("<h1>ERROR</h1>");
+      res.end();
+      console.log("Error garlaa");
+      return;
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write(data);
+    res.end();
+    console.log("Amjilttai duuslaa");
+  });
+  console.log("File unshsanii daraa");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}/`);
+});
