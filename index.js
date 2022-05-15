069473127857adcf4dfff9ff70883d0e86c2a212
Dakiee <ldavaanyam@gmail.com>
2022-05-15 16:19:11 +0800

    back-json

--- index.js
+++ index.js
@@ -28,6 +28,38 @@ const server = http.createServer((req, res) => {
   // console.log("File unshsanii daraa");
 
   // #region PG
+
+  // const pool = new Pool({
+  //   host: "localhost",
+  //   port: "5432",
+  //   database: "News",
+  //   user: "postgres",
+  //   password: "123",
+  // });
+
+  // pool.query(`SELECT * FROM "medee"`),
+  //   (err, data) => {
+  //     res.write(`<html><head>
+  //     <meta charset="UTF-8">
+  //     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //     <title>Welcome</title>
+  // </head><body><table>`);
+  //     for (const row of data.rows) {
+  //       res.write(
+  //         `<tr><td>${row.title}</td><td>` +
+  //           row.news +
+  //           `</td></tr>` +
+  //           row.onsar +
+  //           `</td></tr>`
+  //       );
+  //     }
+  //     res.write("</table></body></html>");
+  //     res.end();
+  //   };
+  // #endregion PG
+
+  // #region API
   const pool = new Pool({
     host: "localhost",
     port: "5432",
@@ -36,27 +68,13 @@ const server = http.createServer((req, res) => {
     password: "123",
   });
 
-  pool.query(`SELECT * FROM "medee"`),
+  pool.query(`SELECT json_agg(onsar) FROM Medee;`),
     (err, data) => {
-      res.write(`<html><head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Welcome</title>
-  </head><body><table>`);
-      for (const row of data.rows) {
-        res.write(
-          `<tr><td>${row.title}</td><td>` +
-            row.news +
-            `</td></tr>` +
-            row.onsar +
-            `</td></tr>`
-        );
-      }
-      res.write("</table></body></html>");
+      res.setHeader("Content-type", "application/json");
+      res.write(JSON.stringify(data.rows[0]));
       res.end();
     };
-  // #endregion PG
+  // #endregion API
 });
 
 server.listen(port, hostname, () => {
