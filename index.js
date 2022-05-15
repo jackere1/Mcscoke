525fdd5ab72be242b00322a287c2cedb367552dd
Dakiee <ldavaanyam@gmail.com>
2022-05-15 11:07:31 +0800

    pull

--- index.js
+++ index.js
@@ -10,21 +10,34 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   let urlPath = req.url.split("/")[1];
   console.log("html unshihiin umnu");
-  fs.readFile(urlPath, (err, data) => {
-    if (err) {
-      res.setHeader("Content-type", "text/html");
-      res.write("<h1>ERROR</h1>");
-      res.end();
-      console.log("Error garlaa");
-      return;
-    }
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(data);
-    res.end();
-    console.log("Amjilttai duuslaa");
+  //#region file read
+  // fs.readFile(urlPath, (err, data) => {
+  //   if (err) {
+  //     res.setHeader("Content-type", "text/html");
+  //     res.write("<h1>ERROR</h1>");
+  //     res.end();
+  //     console.log("Error garlaa");
+  //     return;
+  //   }
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write(data);
+  //   res.end();
+  //   console.log("Amjilttai duuslaa");
+  // });
+  // console.log("File unshsanii daraa");
+
+  // #region PG
+  const pool = new Pool({
+    host: "localhost",
+    port: "3000",
+    database: "News",
+    user: "pguser",
+    password: "password",
   });
-  console.log("File unshsanii daraa");
+
+  pool.query(``);
+  // #endregion PG
 });
 
 server.listen(port, hostname, () => {
