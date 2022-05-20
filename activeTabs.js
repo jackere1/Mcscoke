d06d52c9c7020bbba7830bbe94de6f2e0a440060
Enkhbold <91013518+jackere1@users.noreply.github.com>
2022-05-20 13:45:26 +0800

    mergin physically meh

--- activeTabs.js
+++ activeTabs.js
@@ -2,11 +2,11 @@ function activeTabs() {
   var btns = document.getElementsByClassName("btn");
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
+      var current = document.getElementsByClassName("tabActive");
       if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
+        current[0].className = current[0].className.replace(" tabActive", "");
       }
-      this.className += " active";
+      this.className += " tabActive";
     });
   }
 }
